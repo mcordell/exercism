3296ca0938856408a9b370ea374c45680cf62b07
Michael Cordell <surpher@gmail.com>
2013-08-07 17:05:50 -0700

    iterations 2 and 3 of bob

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -2,11 +2,11 @@ class Bob
 
   def hey(statement_string)
     statement = Statement.new(statement_string) 
-    if statement.is_nothing?
+    if statement.nothing?
       'Fine. Be that way!'
-    elsif statement.is_shouted?
+    elsif statement.shout?
       'Woah, chill out!'
-    elsif statement.is_question?
+    elsif statement.question?
       'Sure.' 
     else
       'Whatever.'
@@ -17,20 +17,20 @@ end
 
 
 class Statement 
-    def initialize(statement_string)
-        @statement = statement_string 
-    end
+  def initialize(statement_string)
+    @statement = statement_string 
+  end
 
-    def is_question?
-        @statement[-1,1] == '?'
-    end
+  def question?
+    @statement.end_with? '?'
+  end
 
-    def is_shouted?
-        @statement.upcase == @statement
-    end
+  def shout?
+    @statement.upcase == @statement
+  end
 
-    def is_nothing?
-      @statement.nil? || @statement.lstrip.empty? 
-    end 
+  def nothing?
+    @statement.nil? || @statement.lstrip.empty? 
+  end 
 
 end
