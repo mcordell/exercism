5ee406d665f2164f4e99e7078b586479428aab2a
Michael Cordell <surpher@gmail.com>
2013-08-09 10:55:10 -0700

    2: refactored out punctation method in favor of scan

--- ruby/word-count/phrase.rb
+++ ruby/word-count/phrase.rb
@@ -4,13 +4,9 @@ class Phrase
       @phrase = phrase
   end
 
-  def without_punctation
-      @phrase.gsub /[^A-Za-z0-9\s]/, ' '
-  end
-
   def word_count
-    all_words = self.without_punctation.downcase.split 
-    word_counts = Hash.new { |h,k| h[k] = 0 }
+    all_words = @phrase.downcase.scan(/\w+/)
+    word_counts = Hash.new(0)
     all_words.each { |word| word_counts[word] += 1 }
     word_counts
   end
