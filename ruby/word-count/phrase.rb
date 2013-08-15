a07a0b81b0459f6ea593e543f81565fab503f346
Michael Cordell <surpher@gmail.com>
2013-08-15 13:49:01 -0700

    submission 6, improving read-ability

--- ruby/word-count/phrase.rb
+++ ruby/word-count/phrase.rb
@@ -1,14 +1,18 @@
 class Phrase
 
   def initialize(phrase)
-      @phrase = phrase
+    @phrase = phrase
   end
 
   def word_count
-    all_words = @phrase.downcase.scan(/\w+/)
-    word_counts = Hash.new(0)
-    all_words.each { |word| word_counts[word] += 1 }
-    word_counts
+    all_words.each_with_object(Hash.new(0)) do |word,hash|
+       hash[word] += 1
+    end
   end
+
+  private
+    def all_words
+      @phrase.downcase.scan(/\w+/)
+    end
 
 end
